97c1748bc41be2e11a8ea7ec48521929327a812d
鹤仙 <guoyunhe.gyh@alibaba-inc.com>
2021-07-09 10:40:14 +0800

    initialize

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  preset: "ts-jest",
+  setupFilesAfterEnv: ["<rootDir>/setupTests.ts"],
+  testEnvironment: "jsdom",
+};
---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Anchor from "@alifd/biz-anchor";
+
+export default function MyComponent() {
+  return (
+    <div>
+      Hello World
+      <Anchor></Anchor>
+    </div>
+  );
+}
